af6a66952a313e39470db22c2198e65f0c26efa3
RamiJeong <rami@performars.com>
2024-06-05 14:34:37 +0900

    complete

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This repository is for the Integrating With HubSpot I: Foundations course. This 
 
 To read the full directions, please go to the [practicum instructions](https://app.hubspot.com/academy/l/tracks/1092124/1093824/5493?language=en).
 
-**Put your HubSpot developer test account custom objects URL link here:** https://app.hubspot.com/contacts/46369063/objects/0-1/views/all/list
+**Put your HubSpot developer test account custom objects URL link here:**
+https://app.hubspot.com/contacts/l/objects/2-30943127/views/all/list
+https://app.hubspot.com/contacts/46369063/objects/2-30943127/views/all/list
 
 ---
 
--- index.js
+++ index.js
@@ -1,26 +1,78 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const app = express()
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.set('view engine', 'pug')
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+  console.log('app.get(/) 진입')
+  const getMoviesRecords = `https://api.hubapi.com/crm/v3/objects/movies?properties=title,score,memo`
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json',
+  }
+  try {
+    const resp = await axios.get(getMoviesRecords, { headers })
+    const results = resp.data.results
+    //results는 객체의 배열
+    for (const element of results) {
+      console.log(element)
+    }
+    res.render('homepage', {
+      title: 'Home | HubSpot APIs',
+      results: results,
+    })
+  } catch (err) {
+    console.error(err)
+  }
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+  console.log('app.get(/update-cobj) 진입')
+
+  try {
+    res.render('updates', {
+      title:
+        'Update Custom Object Form | Integrating With HubSpot I Preacticum',
+    })
+  } catch (err) {
+    console.error(err)
+  }
+})
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+  const update = {
+    properties: {
+      title: req.body.newTitle,
+      score: req.body.newScore,
+      memo: req.body.newMemo,
+    },
+  }
+  //   const updateMovies = `https://api.hubapi.com/crm/v3/objects/2-30943127`
+  const updateMovies = `https://api.hubapi.com/crm/v3/objects/movies`
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json',
+  }
+  try {
+    await axios.post(updateMovies, update, { headers })
+    res.redirect('/')
+  } catch (err) {
+    console.error(err)
+  }
+})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -66,6 +118,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log('Listening on http://localhost:3000'))
